babbb0d18a0e52ba1822fe9a60ed7bff72fb5f31
John Chynoweth <jchynowe1@yahoo.com>
2015-10-14 22:20:28 -0400

    'baseline'

---
+++ test/test.spec.js
@@ -0,0 +1,10 @@
+	var assert = require("assert");
+	
+	describe("First Tests", function() {
+		describe("Get something to work", function() {
+			it("first test", function() {
+				let foo = {bar: "dd"}
+				assert(foo);
+			})
+		});
+	});
