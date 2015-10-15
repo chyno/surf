c83ebc6cbf77632a2ce676e6ff506030ef67bc7b
John Chynoweth <jchynowe1@yahoo.com>
2015-10-14 22:33:43 -0400

    'es6'

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,10 +1,11 @@
 	var assert = require("assert");
+	import {Foo} from './foo';
 	
 	describe("First Tests", ()  => {
 		describe("Get something to work", () => {
 			it("first test", () => {
 				let name = 'john';
-				let foo = {bar: name};
+				let foo = new Foo(name);
 				assert(foo.bar === name);
 			});
 		});
