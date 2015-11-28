fc5dd25859ddd3646995835abd60a6f1f163821a
John Chynoweth <jchynowe1@yahoo.com>
2015-11-28 11:16:26 -0500

    first test

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,44 +1,25 @@
 	var assert = require("assert");
 	import {SurfLookup} from '../processes/surf-lookup';
 	import {SurfReport} from '../models/surf-report';
+	import { cords, forcast } from '../lib/forcast';
 	
 	describe("As a surfer I wan a forcast based on zip", ()  => {
-		var surfReport, result;
+		 
 		
 		before(() => {
-			surfReport = new SurfLookup();
-			 result = surfReport.getSurfReport('22207');
+			 
 		});
 		
 		describe("Get a valid Surf report given a valid zip", () => {
-			it("get a report object", () => {
+			it("get a cords object object", () => {
+				var result = cords('22207');
 				assert(result);
-			} );
-			
-			it("Surf report has temperature", () => {
-				
-				assert(result.temp);
-			} );
-			
-			it("Surf report has direction", () => {
-				assert(result.direction);
 			} ); 
 			
-			it("Surf report has gust", () => {
-				assert(result.gust);
+			it("get a forcast object object", () => {
+				var result = forcast('12', '33');
+				assert(result);
 			} ); 
 		});
-		
-		describe("Get a valid Surf report given based on previos zip", () => {
-			 
-			
-			it("Surf report has temperature", () => {
-				
-				 var lastZip = surfReport.GetLastZip("foo@yahoo.com");
-				 assert(lastZip);
 				 
-			} );
-			
-			 
-		});
 	});
